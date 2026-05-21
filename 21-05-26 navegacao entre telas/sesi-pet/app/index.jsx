0fb278d1568ba35e024680d60865a1018063746d
noemi <noemi.s.oliveira@aluno.senai.br>
2026-05-21 11:03:20 -0300

    sesi-pet

---
+++ 21-05-26 navegacao entre telas/sesi-pet/app/index.jsx
@@ -0,0 +1,10 @@
+import { View } from 'react-native'
+
+export default function Index() {}
+
+  return (
+    <View>
+
+    </View>  
+  )
+
---
+++ 21-05-26 navegacao entre telas/sesi-pet/app/routes/index.js
@@ -0,0 +1,3 @@
+import {CreateNativeStackNavigator} from '@react-navigation/stack'
+
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
